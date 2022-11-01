620fe997c2451620b58da188a69a64b4b18536cd
y-sasakik <71587373+y-sasakik@users.noreply.github.com>
2022-11-01 10:37:09 +0900

    Delete web3 directory

--- web3/js/script.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $('.hamburger').on('click', function() {
-    $('body,.title,.logo,.hamburger, .hamburger span:nth-child(1), .hamburger span:nth-child(2),.hamburger span:nth-child(3),.logo,#navi,.btn,main,footer,body').toggleClass('active');
-    });
-    });
-
