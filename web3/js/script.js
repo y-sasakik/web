8eaf57780ac4040b9b7f864ca547eae17faba76a
y-sasakik <71587373+y-sasakik@users.noreply.github.com>
2022-11-01 10:37:40 +0900

    Add files via upload

---
+++ web3/js/script.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('.hamburger').on('click', function() {
+    $('body,.title,.logo,.hamburger, .hamburger span:nth-child(1), .hamburger span:nth-child(2),.hamburger span:nth-child(3),.logo,#navi,.btn,main,footer,body').toggleClass('active');
+    });
+    });
+
